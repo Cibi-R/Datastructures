99fdec96f8f1a0cdcf39d245f9e7b819ccc0f1af
Cibi R <noo6.programmer@gmail.com>
2021-09-23 23:38:01 +0530

    updated circular list logic, headnode points to last node

--- Project/lib/list/src/circular_list.c
+++ Project/lib/list/src/circular_list.c
@@ -41,35 +41,25 @@ unsigned char MyCircularList_PushElement(MyCircularSinglyList* HeadNode, void* E
 				/*< copy th element */
 				memcpy(newNode->Element, Element, (uint16_t)HeadNode->Element);
 
+				/*< tempNode points to the root node */
 				tempNode = HeadNode;
 
-				/*< root node point to the first inserted node of the list */
+				/*< if the list is empty, make the new node points to itself */
 				if (NULL == tempNode->Next)
 				{
-					/*< list is empty, make root node point to the newnode */
-					HeadNode->Next = newNode;
-
 					/*< make new node point to itself, create a circular list */
 					newNode->Next = newNode;
 				}
-				/*< list is not empty */
 				else
 				{
-					/*< point to the first node */
-					tempNode = tempNode->Next;
+					/*< last node always points to the first node now, newNode is the last node */
+					newNode->Next = (tempNode->Next)->Next;
 
-					/*< last node will point to the first node so, run the loop till it points the first node */
-					while (HeadNode->Next != tempNode->Next)
-					{
-						tempNode = tempNode->Next;
-					}
-
-					/*< link the node */
-					tempNode->Next = newNode;
-
-					/*< make it circular by linking the last node to first node */
-					newNode->Next = HeadNode->Next;
+					/*< make (last - 1)th node points to last node */
+					(tempNode->Next)->Next = newNode;
 				}
+
+				HeadNode->Next = newNode;
 
 				retVal = 1;
 			}
@@ -89,38 +79,41 @@ unsigned char MyCircularList_PopElement(MyCircularSinglyList* HeadNode, void* El
 
 	MyCircularSinglyList* tempNode = NULL;
 
+	MyCircularSinglyList* lastNode = NULL;
+
 	if (NULL != HeadNode)
 	{
 		if (NULL != HeadNode->Next)
 		{
-			tempNode = HeadNode;
+			tempNode = HeadNode->Next;
 
-			if (tempNode->Next == (tempNode->Next)->Next)
+			while (tempNode->Next != HeadNode->Next)
 			{
-				/*< in the next instruction we are setting this value to NULL */
-				tempNode = HeadNode->Next;
+				tempNode = tempNode->Next;
+			}
 
-				/*< only one element present inside the list */
+			lastNode = tempNode->Next;
+
+			/*< copy the value */
+			memcpy(Element, lastNode->Element, (uint16_t)HeadNode->Element);
+
+			/*< if tempNode points to itself then only one node present in the list */
+			if (tempNode->Next == tempNode)
+			{
 				HeadNode->Next = NULL;
 			}
 			else
 			{
-				while (HeadNode->Next != (tempNode->Next)->Next)
-				{
-					tempNode = tempNode->Next;
-				}
+				/*< change the HeadNode pointer */
+				HeadNode->Next = tempNode;
+
+				/*< relink the last node, last node points to first node */
+				tempNode->Next = lastNode->Next;
 			}
 
-			printf_s("value : %d\n", *((unsigned int*)(tempNode->Next)->Element));
+			free(lastNode->Element);
 
-			memcpy(Element, (tempNode->Next)->Element, (uint16_t)HeadNode->Element);
-
-			free(tempNode->Next->Element);
-
-			free(tempNode->Next);
-
-			/*< point to the first node */
-			tempNode->Next = HeadNode->Next;
+			free(lastNode);
 
 			retVal = 1;
 		}
@@ -131,65 +124,38 @@ unsigned char MyCircularList_PopElement(MyCircularSinglyList* HeadNode, void* El
 
 unsigned char MyCircularList_InsertElement(MyCircularSinglyList* HeadNode, void* Element, uint16_t Position)
 {
-	uint16_t currPosition = 0;
+	unsigned char retVal = 0;
 
+	uint16_t currPosition = 0u;
+
+	/*< list is not created */
 	if (NULL != HeadNode)
 	{
+		/*< list is empty */
 		if (NULL != HeadNode->Next)
 		{
-			MyCircularSinglyList* tempNode = HeadNode;
-
-			while ((((tempNode->Next)->Next) != HeadNode->Next) && (Position < currPosition))
-			{
-				tempNode = tempNode->Next;
-
-				currPosition++;
-			}
-
-			if (currPosition == Position)
-			{
-				MyCircularSinglyList* newNode = (MyCircularSinglyList*) malloc(sizeof(MyCircularSinglyList));
-
-				if (NULL != newNode)
-				{
-					newNode->Element = (void*)malloc((uint16_t)HeadNode->Element);
-
-					if (NULL != newNode->Element)
-					{
-						if (0u == Position)
-						{
-							MyCircularSinglyList* lastNode = HeadNode;
-
-							/*< if the position is 0 we should alter the next of the last element */
-							while (((lastNode->Next)->Next) != HeadNode->Next)
-							{
-								lastNode = lastNode->Next;
-
-								currPosition++;
-							}
-
-							lastNode->Next->Next = newNode;
-						}
-						
-						memcpy(newNode->Element, Element, (uint16_t)HeadNode->Element);
-
-						newNode->Next = tempNode->Next;
-
-						tempNode->Next = newNode->Next;
-					}
-					else
-					{
-						free(newNode);
-					}
-				}
-			}
+			
 		}
 	}
+
+	return retVal;
 }
 
-unsigned char MyCircularList_RemoveElement(MyCircularSinglyList* List, void* Element, uint16_t Position)
+unsigned char MyCircularList_RemoveElement(MyCircularSinglyList* HeadNode, void* Element, uint16_t Position)
 {
+	unsigned char retVal = 0;
 
+	uint16_t currPosition = 0u;
+
+	/*< list is not created */
+	if (NULL != HeadNode)
+	{
+		/*< list is empty */
+		if (NULL != HeadNode->Next)
+		{
+			
+		}
+	}
 }
 
 unsigned char MyCiruclarList_Traverse(MyCircularSinglyList* HeadNode)
